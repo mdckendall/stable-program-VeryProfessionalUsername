b55d1150f284f6c39588948b8e0985fb2e540136
VeryProfessionalUsername <125950866+VeryProfessionalUsername@users.noreply.github.com>
2023-04-28 11:52:03 -0400

    Update Stable.java

--- Stable.java
+++ Stable.java
@@ -1,1 +1,17 @@
-//Create the Stable class here.
+import java.util.*;
+public class Stable {
+  public String address;
+  public ArrayList<Horse> arrayList;
+
+  Stable() {
+    arrayList = new ArrayList<>();
+  }
+  
+  public String toString() {
+    String result = "";
+    for (int i = 0; i < arrayList.size(); i++) {
+      result += "Horse #" + (i + 1) + " " + arrayList.get(i).toString();
+    }
+    return result;
+  }
+}
