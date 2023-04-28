c9230ffb4945b9394f567e0042363f1cc0e23661
VeryProfessionalUsername <125950866+VeryProfessionalUsername@users.noreply.github.com>
2023-04-28 12:05:15 -0400

    Update Stable.java

--- Stable.java
+++ Stable.java
@@ -1,17 +1,23 @@
 import java.util.*;
-public class Stable {
-  public String address;
-  public ArrayList<Horse> arrayList;
 
-  Stable() {
-    arrayList = new ArrayList<>();
-  }
-  
-  public String toString() {
-    String result = "";
-    for (int i = 0; i < arrayList.size(); i++) {
-      result += "Horse #" + (i + 1) + " " + arrayList.get(i).toString();
+public class Stable {
+    public String address;
+    public ArrayList<Horse> arrayList;
+
+    public Stable() {
+        arrayList = new ArrayList<>();
     }
-    return result;
-  }
+
+    public String toString() {
+        String output = "";
+        int horseCount = 1;
+
+        for (Horse h : arrayList) {
+            output += "Horse #" + horseCount + " Name: " + h.name + 
+                      " Weight: " + h.weight + " Tame: " + h.tame + "\n";
+                      horseCount++;
+        }
+
+        return output;
+    }
 }
